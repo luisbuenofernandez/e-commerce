5526b44635272232e9f0e5056dd36a804c661f31
Luis Bueno Fernandez <buenofernandezluisfernando@gmail.com>
2022-10-01 23:19:14 -0300

    E4 - Completo!
    FINALIZADO!!!

--- js/boton_usuario.js
+++ js/boton_usuario.js
@@ -6,10 +6,9 @@ const boton_usuario = document.getElementById("nombre_de_usuario");
 const cerrar_sesion = document.getElementById("borrar_usuario");
 
 const dropdown = `<div class="dropdown">
-  <a class="nav-link" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+  <a class="nav-link"  role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
   ${usuario}
   </a>
-
   <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
     <li><a class="dropdown-item" href="my-profile.html">${usuario}</a></li>
     <li><a class="dropdown-item" href="cart.html">Carrito</a></li>
--- js/product-info.js
+++ js/product-info.js
@@ -1,80 +1,75 @@
-// VARIABLES GLOBALES.
 let id_producto = localStorage.getItem('producto_id');
-console.log(id_producto);
+let user_name = localStorage.getItem("nombreUsuario");
 let calificacion_usuario;
 let comentarios_recuperados = [];
 
 /* ....................................................................................................... */
 
-function carousel(images){
+function carousel(images) {     //  Entrega 4 - DESAFÍO:
 
-    return `<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="${images[0]}" class="d-block w-100" alt="...0">
-    </div>
-    <div class="carousel-item">
-      <img src="${images[1]}" class="d-block w-100" alt="...1">
-    </div>
-    <div class="carousel-item">
-      <img src="${images[2]}" class="d-block w-100" alt="...2">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-  </div>`;
+    let fotos = `<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img src="${images[0]}" class="d-block w-100" alt="">
+                      </div>`
 
+    for (let i = 1; i < images.length; i++) {
+        fotos += `<div class="carousel-item">
+                        <img src="${images[i]}" class="d-block w-100" alt="">
+                      </div>`
+    }
+
+    fotos += `</div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Next</span>
+                    </button>
+                    </div>`;
+    return fotos;
 }
 
+/* ....................................................................................................... */
+
 function productoInfo(info) {   // ENTREGA 3.2: TOMAR ID DEL PRODUCTO Y MOSTRAR DETALLES DEL MISMO.
-    let producto_a_agregar = `        
+    let producto_a_agregar = `
+        <div class="text-center p-4">
         <h2> ${info.name}</h2>
-        
-
-        ${carousel(info.images)}
-
-    
+        </div>
+        ${carousel(info.images, info.description)}
+        <br>
         <div>
+        <h5 id="desc_prod">${info.description}</h5><br> 
+            <h5>Categoría</h5>
+            <p> ${info.category}</p>  
             <h5>Precio</h5>
             <p> ${info.currency} ${info.cost}</p>
-            <h5>Descripción</h5>
-            <p> ${info.description}</p>            
-            <h5>Categoría</h5>
-            <p> ${info.category}</p>            
             <h5>Cantidad de vendidos</h5>
             <p> ${info.soldCount} vendidos. </p>            
         </div>`;
 
     document.getElementById('datos_producto').innerHTML += producto_a_agregar;
     ver_relacionados(info);
-    /* for (let i = 0; i < info.images.length; i++) {
-        producto_a_agregar += `<img src='${info.images[i]}' href=''>`;
-
-    } */
 }
 
 /* ....................................................................................................... */
 
 function total_comentarios(comentarios) {     // ENTREGA 3.3: TRAER Y MOSTRAAR COMENTARIOS DEL PRODUCTO.
-    let comentario_cliente = ``;
 
-    for (let comentario of comentarios) {
-        if (comentario !== null) {
-            comentario_cliente += `<div>
-            <strong>${comentario.user} -</strong> ${stars(comentario.score)}<br>
-            - ${comentario.dateTime} -  <br>
-            ${comentario.description}
+    for (let i = 0; i < comentarios.length; i++) {
+
+        if (comentarios[i] !== null) {
+            let comentario_cliente = `<div>
+            <p><strong>${comentarios[i].user} - </strong>${stars(comentarios[i].score)}</p>
+            <small>${comentarios[i].dateTime} </small>
+            <p>${comentarios[i].description}</p>
             </div>`;
+            document.getElementById('comentarios_totales').innerHTML += comentario_cliente;
         }
     }
-
-    document.getElementById('comentarios_totales').innerHTML += comentario_cliente;
 }
 
 /* ....................................................................................................... */
@@ -85,14 +80,13 @@ function stars(puntos) {     // CALIFICACIÓN DEL PRODUCTO CON ESTRELLAS.
         puntos = 5;
     }
 
-    for (i = 1; i <= 5; i++) {
+    for (let i = 1; i <= 5; i++) {
         if (i <= puntos) {
             estrellas += `<span class="fa fa-star checked" id=${i}></span>`;
         } else {
             estrellas += `<span class="fa fa-star" id=${i}></span>`;
         }
     }
-
     return estrellas;
 }
 
@@ -103,23 +97,37 @@ function caja_de_comentario() {     // SI USER EN LOCALSTORAGE, MOSTRAR CAJA DE 
     let comentario_usuario;
     if (localStorage.getItem('nombreUsuario')) {
 
-        comentario_usuario = `<form action="" id="">
-            <fieldset>
-                <legend>
-                    <h5>${usuario}</h5><br>
-                    <div id="estrellas_usuario">`;
+        comentario_usuario = ` 
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                Comentar
+            </button>
+            
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 id="modalComentar">${user_name} -   </h5>
+                            <div id="estrellas_usuario" >`;
 
-        for (let i = 1; i <= 5; i++) {
-            comentario_usuario += `<span class="fa fa-star checked" id="${i}"></span>`;
-        }
+                for (let i = 1; i <= 5; i++) {
+                    comentario_usuario += `<span class="fa fa-star checked" id="${i}"></span>`;
+                }
 
-        comentario_usuario += `</div>
-                </legend>             
-                <textarea class="comentario" id="comentario_user" placeholder="Ingrese el texto deseado..."></textarea>
-                <div id='error_textarea'></div>
-                <input type="button" value="Comentar" id="btn_comentar">
-        <fieldset>
-        </form>`;
+                comentario_usuario += `</div>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body ">
+                                <textarea class="comentario" id="comentario_user" placeholder="Ingrese el texto deseado..."></textarea>
+                                <div class="container">
+                                    <div id='error_textarea'>
+                                    </div>
+                                </div>
+                                <input type="button" class="btn btn-primary col-12 " value="Enviar" id="btn_comentar">
+                            </div>
+                        </div>   
+                    </div>
+                </div>
+                </div>`;
 
         document.getElementById('caja_de_comentario').innerHTML = comentario_usuario;
         usuario_comentar();
@@ -191,11 +199,12 @@ function usuario_comentar() {       // ENTREGABLE 3: DESAFÍO:
         let user_comment;
         if (texto_comentario.value != '') {
             user_comment = {
-                dateTime: date_time(),                  // fecha_actual + ' ' + hora_actual,
-                description: texto_comentario.value,
+
                 product: id_producto,
                 score: calificacion_usuario,
-                user: localStorage.getItem("nombreUsuario")
+                description: texto_comentario.value,
+                user: user_name,
+                dateTime: date_time(),                  // fecha_actual + ' ' + hora_actual,
             };
             comentario_sin_texto.innerHTML = '';
             calificacion_usuario = 5;
@@ -228,31 +237,42 @@ function recuperar_comentarios() {      // // ENTREGABLE 3: DESAFÍO - Mostrar c
 function ver_relacionados(prod_actual) {    //  ENTREGA - 4.1: MOSTRAR PRODUCTOS RELACIONADOS.
 
     let prod_relacionados = document.getElementById("prod_relacionados");
-    let anterior_posterior = `<div class="container">
-        <div class="row">
-        <div class="col"></div>
-          <div class="col">
-          <figure id="${prod_actual.relatedProducts[1].id}">
-          <img src="${prod_actual.relatedProducts[1].image}" alt="${prod_actual.relatedProducts[1].name}">
-          <figcaption>${prod_actual.relatedProducts[1].name}</figcaption>
-          </figure>
-          </div>
-          <div class="col">
-          <figure id="${prod_actual.relatedProducts[0].id}">
-          <img src="${prod_actual.relatedProducts[0].image}" alt="${prod_actual.relatedProducts[0].name}">
-          <figcaption>${prod_actual.relatedProducts[0].name}</figcaption>
-          </figure>
-          </div>
-        </div>
-      </div>`;
+    let anterior_posterior = `
+            <button type="button" class="btn btn-outline-default" data-bs-toggle="modal" data-bs-target="#ver_relacionados">
+                Relacionados
+            </button>
+            <div class="modal fade" id="ver_relacionados" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="staticBackdropLabel">Productos relacionados:</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="container">
+                            <div class="modal-body row">`;      
+
+    for (let relacionado of prod_actual.relatedProducts) {
+
+        anterior_posterior += `<div class="card col w-100" id="${relacionado.id}">
+        <img src="${relacionado.image}" alt="${relacionado.name}" class="card-img-top">
+            <div class="card-body">
+                <p class="card-text">${relacionado.name}</p>
+            </div>
+        </div>`;
+    }
+    
+    anterior_posterior += `</div>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
 
     prod_relacionados.innerHTML = anterior_posterior;
 
-    let elementos_relacionados = document.getElementsByTagName('figure');
-    console.log(elementos_relacionados);
-    for (let elemento of elementos_relacionados) {
-        elemento.addEventListener('click', () => {
-            localStorage.setItem('producto_id', elemento.id);
+    let elementos_relacionados = document.getElementsByClassName('card');
+    for (let card of elementos_relacionados) {
+        card.addEventListener('click', () => {
+            localStorage.setItem('producto_id', card.id);
             window.location = 'product-info.html';
         })
     }
